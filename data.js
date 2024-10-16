987c80e7419befa004f0b023eb3fa2fc697d792d
riccardoluise <riccardo.luise.info@gmail.com>
2024-10-16 19:01:04 +0200

    add data

---
+++ data.js
@@ -0,0 +1,138 @@
+const mindMapData = {
+    "name": "Web Development",
+    "children": [
+        {
+            "name": "Frontend Development",
+            "children": [
+                {"name": "HTML"},
+                {"name": "CSS"},
+                {"name": "JavaScript"},
+                {"name": "TypeScript"},
+                {
+                    "name": "Frameworks e Librerie",
+                    "children": [
+                        {"name": "React"},
+                        {"name": "Angular"},
+                        {"name": "Vue.js"},
+                        {"name": "Svelte"},
+                        {"name": "jQuery"},
+                        {"name": "Bootstrap"},
+                        {"name": "Tailwind CSS"}
+                    ]
+                },
+                {
+                    "name": "Build Tools",
+                    "children": [
+                        {"name": "Webpack"},
+                        {"name": "Vite"},
+                        {"name": "Rollup"},
+                        {"name": "Babel"}
+                    ]
+                },
+                {
+                    "name": "Testing",
+                    "children": [
+                        {"name": "Jest"},
+                        {"name": "Cypress"},
+                        {"name": "Playwright"},
+                        {"name": "Selenium"}
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Backend Development",
+            "children": [
+                {
+                    "name": "Linguaggi",
+                    "children": [
+                        {"name": "Node.js"},
+                        {"name": "Python"},
+                        {"name": "Ruby"},
+                        {"name": "PHP"},
+                        {"name": "Java"},
+                        {"name": "Go"},
+                        {"name": "Rust"},
+                        {"name": "C#"}
+                    ]
+                },
+                {
+                    "name": "Frameworks",
+                    "children": [
+                        {"name": "Express.js"},
+                        {"name": "Django"},
+                        {"name": "Ruby on Rails"},
+                        {"name": "Laravel"},
+                        {"name": "Spring Boot"},
+                        {"name": "ASP.NET Core"}
+                    ]
+                },
+                {
+                    "name": "API",
+                    "children": [
+                        {"name": "REST"},
+                        {"name": "GraphQL"},
+                        {"name": "gRPC"},
+                        {"name": "WebHooks"}
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Databases",
+            "children": [
+                {
+                    "name": "Relazionali (SQL)",
+                    "children": [
+                        {"name": "PostgreSQL"},
+                        {"name": "MySQL"},
+                        {"name": "SQLite"},
+                        {"name": "Oracle"}
+                    ]
+                },
+                {
+                    "name": "Non Relazionali (NoSQL)",
+                    "children": [
+                        {"name": "MongoDB"},
+                        {"name": "Redis"},
+                        {"name": "Cassandra"},
+                        {"name": "Elasticsearch"}
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "DevOps e CI/CD",
+            "children": [
+                {
+                    "name": "Containerizzazione",
+                    "children": [
+                        {"name": "Docker"},
+                        {"name": "Kubernetes"}
+                    ]
+                },
+                {
+                    "name": "CI/CD Tools",
+                    "children": [
+                        {"name": "Jenkins"},
+                        {"name": "GitLab CI"},
+                        {"name": "GitHub Actions"},
+                        {"name": "CircleCI"}
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Cloud e Hosting",
+            "children": [
+                {"name": "AWS"},
+                {"name": "Google Cloud"},
+                {"name": "Azure"},
+                {"name": "Heroku"},
+                {"name": "Netlify"},
+                {"name": "Vercel"},
+                {"name": "GitHub Pages"}
+            ]
+        }
+    ]
+};
