240270bc131b6103e0e38e8a4b082006dc2b6c6a
riccardoluise <riccardo.luise.info@gmail.com>
2024-10-16 19:30:25 +0200

    Add center buttons

--- app.js
+++ app.js
@@ -240,6 +240,34 @@ simulation.on("tick", () => {
         .attr("transform", d => `translate(${d.x},${d.y})`);
 });
 
+function centerView() {
+    const rootNode = root; // Assumiamo che 'root' sia accessibile globalmente
+    
+    svg.transition().duration(750).call(
+        zoom.transform,
+        d3.zoomIdentity
+          .translate(width / 2, height / 2)
+          .scale(1)
+          .translate(-rootNode.x, -rootNode.y)
+    );
+}
+
+function fitView() {
+    const bounds = g.node().getBBox();
+    const fullWidth = bounds.width;
+    const fullHeight = bounds.height;
+    const midX = bounds.x + fullWidth / 2;
+    const midY = bounds.y + fullHeight / 2;
+
+    const scale = 0.9 / Math.max(fullWidth / width, fullHeight / height);
+    const translate = [width / 2 - scale * midX, height / 2 - scale * midY];
+
+    svg.transition().duration(750).call(
+        zoom.transform,
+        d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale)
+    );
+}
+
 d3.select("#zoom-in").on("click", () => {
     svg.transition().call(zoom.scaleBy, 1.3);
 });
@@ -257,3 +285,7 @@ d3.select("#collapse-all").on("click", () => {
     root.children.forEach(collapseNode);
     update(root);
 });
+
+d3.select("#center-view").on("click", centerView);
+
+d3.select("#fit-view").on("click", fitView);
